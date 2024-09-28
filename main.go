c0dc205738514476d14bc9c8334ffb3583e8bac0
nlsc1999 <nlsc1999@gmail.com>
2024-09-28 04:20:57 +0000

    demo

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Item struct {
@@ -16,12 +22,66 @@ type Item struct {
 	Updated_at  time.Time `json:"updated_at"`
 }
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	// Connect database
+	db, err := gorm.Open(postgres.Open(os.Getenv("CONNECTION_STRING")), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(db)
+
+	// Create APIs
 	r := gin.Default()
+
+	api := r.Group("v1")
+	{
+		items := api.Group("items")
+		items.POST("", CreateItem(db))
+		items.GET("")
+		items.GET("/:id")
+		items.PUT("/:id")
+		items.DELETE("/:id")
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.Run()
+}
+
+func CreateItem(db *gorm.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		item := Item{}
+
+		if err := c.ShouldBind(&item); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		item.ID = uuid.New()
+		item.Created_at = time.Now()
+		item.Updated_at = time.Now()
+
+		if err := db.Create(&item).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"data": item.ID,
+		})
+	}
 }
